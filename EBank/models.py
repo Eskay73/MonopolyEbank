59481a2493262ef4377b772c07368e7194d7dfb1
Eskay73 <eskayark99@gmail.com>
2022-01-08 23:31:49 +0530

    added players and games models

--- EBank/models.py
+++ EBank/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import datetime
+class Player(models.Model):
+    playerName = models.CharField(max_length=50)
+    money = models.PositiveIntegerField()
+    getOutOFJail = models.BooleanField()
+    properties  = models.ForeignKey(to=Property, on_delete = models.CASCADE)
+    game  = models.ForeignKey(to=Game, on_delete = models.CASCADE)
 
-# Create your models here.
+
+class Game(models.Model):
+    Gamename = models.CharField(max_length=50)
+    Result = models.CharField(max_length=10)
+    createdAt = models.DateTimeField(default=datetime.now, blank=True)
