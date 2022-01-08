1025c8a63a4c4e176e4b4751fc50f570382c8e9a
Inika Roy <inika.19298@gmail.com>
2022-01-08 21:37:33 +0530

    add property model

--- EBank/models.py
+++ EBank/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class player(models.Model):
+    pass
+
+class property(models.Model):
+    propertyName = models.TextField()
+    cost = models.IntegerField()
+    rent =  models.IntegerField()
+    color =  models.TextField()
+    buildings : models.IntegerField()
+    Owner : models.TextField()
