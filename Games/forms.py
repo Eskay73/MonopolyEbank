071847ae6eb1e7bb900a50efcaee2e37c44189df
Eskay73 <eskayark99@gmail.com>
2022-01-16 12:44:12 +0530

    new templates

--- Games/forms.py
+++ Games/forms.py
@@ -1,9 +1,18 @@
 from django import forms
 from django.forms import ModelForm
 from EBank.models import player
+from .models import games
+
 
 
 class playerForm(forms.ModelForm):
     class Meta:
         model = player
         fields=('playerName',)
+
+
+class gameForm(forms.ModelForm):
+
+    class Meta:
+        model = games
+        fields=('gameName',)
