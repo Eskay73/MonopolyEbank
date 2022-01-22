2ce041a34218f3ece80dcab8924c3864db8e458f
Inika Roy <inika.19298@gmail.com>
2022-01-22 22:41:11 +0530

    input styling on players page

--- Games/forms.py
+++ Games/forms.py
@@ -13,7 +13,7 @@ class playerForm(forms.ModelForm):
             'playerName': ('Player Name'),
         }
         widgets = {
-            'playerName': forms.TextInput(attrs={'class': 'todo-form appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none',
+            'playerName': forms.TextInput(attrs={'class': 'todo-form bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500', #appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none',
                                                 'id':'playerName',
                                             },),
         }
