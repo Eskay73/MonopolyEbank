d58966242829ac0cdabff26897edb26ea874bf55
Inika Roy <inika.19298@gmail.com>
2022-01-28 22:00:37 +0530

    adds templates to inventory app

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-# from .models import games
+from .models import games
 from EBank.models import player
 # from .forms import playerForm, gameForm
 from django.core.exceptions import ObjectDoesNotExist
